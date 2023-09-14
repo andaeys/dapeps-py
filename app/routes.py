1f57b0ce9cae21caf0b4fc154f1cc5772a88e909
andaeys <ekayudha.sulistianda@quipper.com>
2023-09-14 22:10:18 +0700

    impl get users endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,16 @@
 from flask import jsonify
 from app import app
+from app.models import User
 
 
 @app.route('/hello', methods=['GET'])
 def sample_endpoint():
     data = {'message': 'This is a sample endpoint.'}
     return jsonify(data)
+
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    user_list = [{'id': user.id, 'username': user.username, 'email': user.email} for user in users]
+    return jsonify({'users': user_list})
